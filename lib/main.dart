d57fe0252d085265f7f379733b65c13a4576b121
eduardo <luizzeduardp@gmail.com>
2022-12-19 09:19:57 -0300

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Adath Comandos',
+      title: 'Adath Comandos v2',
       theme: ThemeData(
         useMaterial3: true,
       ),
